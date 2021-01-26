722ff8fc47236c2822222d7b72778380d03dc937
ptr-syncoria <pariket@syncoria.com>
2021-01-27 00:55:26 +0530

    [FIX]: create message change method use message_post

--- chatter_accumulation/models/helpdesk.py
+++ chatter_accumulation/models/helpdesk.py
@@ -20,34 +20,44 @@ class Message(models.Model):
         return res
 
     def _create_accumulation(self, vals):
+        print("_create_accumulation >>>>>>>>>>>>>>", self, vals)
         model_info = ['project.task', 'sale.order', 'res.partner', 'account.move', 'helpdesk.ticket']
         if vals.get('model') in model_info:
             record = self.env[vals.get('model')].search([('id', '=', vals.get('res_id'))])
             print("record >>>>>>>>>>>>>", record)
             if vals.get('model') == 'helpdesk.ticket':
+                print("record.partner_id >>>>>>>>>>>>>>", record.partner_id)
                 if record.partner_id:
                     vals['model'] = 'res.partner'
                     vals['res_id'] = record.partner_id.id
-                    self.create(vals)
+                    record.partner_id.message_post(body=vals.get('body'))
+                    # self.create(vals)
             elif vals.get('model') == 'project.task':
+                print("record.helpdesk_ticket_id >>>>>>>>>>>>>>", record.helpdesk_ticket_id)
                 if record.helpdesk_ticket_id:
                     vals['model'] = 'helpdesk.ticket'
                     vals['res_id'] = record.helpdesk_ticket_id.id
-                    self.create(vals)
+                    record.helpdesk_ticket_id.message_post(body=vals.get('body'))
+                    # self.create(vals)
             elif vals.get('model') == 'sale.order':
+                print("record.helpdesk_ids >>>>>>>>>>>>>", record.helpdesk_ids)
                 if record.helpdesk_ids:
                     for helpdesk in record.helpdesk_ids:
                         vals['model'] = 'helpdesk.ticket'
                         vals['res_id'] = helpdesk.id
-                        self.create(vals)
+                        helpdesk.message_post(body=vals.get('body'))
+                        # self.create(vals)
                 elif record.task_id and record.task_id.helpdesk_ticket_id:
                     vals['model'] = 'helpdesk.ticket'
                     vals['res_id'] = record.task_id.helpdesk_ticket_id.id
-                    self.create(vals)
+                    record.task_id.helpdesk_ticket_id.message_post(body=vals.get('body'))
+                    # self.create(vals)
             elif vals.get('model') == 'account.move':
                 if record.invoice_line_ids and record.invoice_line_ids.mapped('sale_line_ids') and record.invoice_line_ids.mapped('sale_line_ids').mapped('task_id') and record.invoice_line_ids.mapped('sale_line_ids').mapped('task_id').mapped('helpdesk_ticket_id'):
+                    print("record.invoice_line_ids.mapped('sale_line_ids').mapped('task_id').mapped('helpdesk_ticket_id') >>>>>>>>>>>", record.invoice_line_ids.mapped('sale_line_ids').mapped('task_id').mapped('helpdesk_ticket_id'))
                     for helpdesk in record.invoice_line_ids.mapped('sale_line_ids').mapped('task_id').mapped('helpdesk_ticket_id'):
                         vals['model'] = 'helpdesk.ticket'
                         vals['res_id'] = helpdesk.id
-                        self.create(vals)
+                        helpdesk.message_post(body=vals.get('body'))
+                        # self.create(vals)
         return True
