a2150770b33b2842a4a18525e1337ede3bd2d1c7
Toan Truong <toan@syncoria.com>
2021-02-01 09:50:56 -0500

    [Calendar] hide next button when show 1 month

--- rhp_api/static/src/js/rhp_calendar_appointment.js
+++ rhp_api/static/src/js/rhp_calendar_appointment.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
     if (document.getElementById('month_2') !=null) {
         document.getElementById('month_2').style.display = 'none';
-    
+    }
+    else {
+        var nexts = document.getElementsByClassName('btn_next'); 
+        for (var i=0;i<nexts.length;i+=1){
+            nexts[i].style.display = 'none';
+        }
     }
 });
