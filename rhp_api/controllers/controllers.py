c5b488523a30a6dac24857a56d6c4948ba942203
Toan Truong <toan@syncoria.com>
2021-02-03 10:47:29 -0500

    [GetProducts] update link image

--- rhp_api/controllers/controllers.py
+++ rhp_api/controllers/controllers.py
@@ -351,8 +351,8 @@ class RhpApi(http.Controller):
         product_obj = request.env['product.product'].sudo().search([('company_id', '=', company_obj.id)])
         product_array = []
         result = {}
-        path_info = http.request.env['ir.config_parameter'].sudo().get_param('web.base.url')
-
+        # path_info = http.request.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        path_info = 'https://unilux-vfc-rhp-1894585.dev.odoo.com'
         for a in product_obj:
             temp = {}
             if a.default_code:
